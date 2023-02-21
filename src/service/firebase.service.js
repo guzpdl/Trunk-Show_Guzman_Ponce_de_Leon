bce7ea5ad3d0d1ee9ea1bf18ac541b416c4239d5
Guzman <guzpdl@gmail.com>
2023-02-21 18:55:41 +0100

    firebase not working yet

--- src/service/firebase.service.js
+++ src/service/firebase.service.js
@@ -1,18 +1,40 @@
-import * as firebase from "firebase/app";
-import "firebase/firestore";
+// import * as firebase from "firebase/app";
+// import "firebase/firestore";
 
-const app = firebase.initializeApp({
+// const app = firebase.initializeApp({
+//   apiKey: "AIzaSyAt_IDeJZbDokbAlB1i193nrLd4ytysKR0",
+//   authDomain: "fren-supply.firebaseapp.com",
+//   projectId: "fren-supply",
+//   storageBucket: "fren-supply.appspot.com",
+//   messagingSenderId: "447618822651",
+//   appId: "1:447618822651:web:373d8a6bd1830f31ca5b5e",
+// });
+
+// export const getFirebase = () => {
+//   return app;
+// };
+// export const getFirestore = () => {
+//   return firebase.firestore(app);
+// };
+
+import { initializeApp } from "firebase/app";
+import { doc, getDoc, getFirestore } from "firebase/firestore/lite";
+
+const firebaseConfig = {
   apiKey: "AIzaSyAt_IDeJZbDokbAlB1i193nrLd4ytysKR0",
   authDomain: "fren-supply.firebaseapp.com",
   projectId: "fren-supply",
   storageBucket: "fren-supply.appspot.com",
   messagingSenderId: "447618822651",
   appId: "1:447618822651:web:373d8a6bd1830f31ca5b5e",
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+const productRef = doc(db, "productList");
+getDoc(productRef).then((snapshot) => {
+  console.log(snapshot);
 });
 
-export const getFirebase = () => {
-  return app;
-};
-export const getFirestore = () => {
-  return firebase.firestore(app);
-};
+// ESTO NO ESTA FUNCIONANDOOOOOOOOOOOOOOOOOOOOOOOOO
